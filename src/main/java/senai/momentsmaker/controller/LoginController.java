a8ca72d93735483e5f4f352e0fc48398a87abdd9
Jardel Sgrott Nagel <sninfo94@gmail.com>
2020-06-18 22:26:58 -0300

    MM-106 - CORS para todas as origens.

--- src/main/java/senai/momentsmaker/controller/LoginController.java
+++ src/main/java/senai/momentsmaker/controller/LoginController.java
@@ -7,13 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +15,7 @@ import senai.momentsmaker.entity.LoginEntity;
 import senai.momentsmaker.repository.LoginRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class LoginController {
 
